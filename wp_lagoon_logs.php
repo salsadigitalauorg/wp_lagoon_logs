9c3ab7e40ca44bddb454890d803e1f754e9d7237
Govind Kumar Malu <govind@salsadigital.com.au>
2021-01-30 11:20:55 +0530

    Added init hook

--- wp_lagoon_logs.php
+++ wp_lagoon_logs.php
@@ -27,24 +27,26 @@ if (!defined( 'Inpsyde\Wonolog\LOG')) {
   return;
 }
 
-/**
- * Activation hook.
- */
-function wp_lagoon_logs_activation_actions(){
-  do_action('wp_lagoon_logs_extension_activation');
-}
-
-register_activation_hook( __FILE__, 'wp_lagoon_logs_activation_actions' );
 // Set default values here
 function wp_lagoon_logs_default_settings() {
   $default = [
     'll_settings_logs_host' => 'application-logs.lagoon.svc',
     'll_settings_logs_port' => 5140,
-    'll_settings_logs_identifier' => 'drupal',
+    'll_settings_logs_identifier' => 'wordpress',
   ];
   update_option('wp_ll_settings', $default);
 }
-add_action('wp_lagoon_logs_extension_activation', 'wp_lagoon_logs_default_settings');
+
+/**
+ * Plugin init action because activation hook won't trigger in MU plugin.
+ */
+function wp_lagoon_logs_extension_init() {
+  if (get_option( 'wp_ll_settings')) {
+    return;
+  }
+  wp_lagoon_logs_default_settings();
+}
+add_action('init', 'wp_lagoon_logs_extension_init');
 
 if (getenv('LAGOON_ENVIRONMENT_TYPE') && getenv('LAGOON_ENVIRONMENT_TYPE') !== 'local') {
   $options = get_option('wp_ll_settings');
