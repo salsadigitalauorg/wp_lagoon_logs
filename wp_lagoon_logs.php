9e3119f764412fe5a43d34a2dc6819dca573fe92
Ivan Grynenko <ivan.grynenko@gmail.com>
2025-04-16 13:40:43 +1000

    updated plugin version and added authors; improved installation checks for database readiness

--- wp_lagoon_logs.php
+++ wp_lagoon_logs.php
@@ -2,8 +2,8 @@
 /**
  * Plugin Name: WP lagoon logs
  * Description: Simple wonolog wrapper for Lagoon.
- * Version: 0.2
- * Author: Govind Maloo
+ * Version: 0.5
+ * Authors: Govind Maloo, Ivan Grynenko
  * Author URI: http://drupal.org/u/govind.maloo
  * License: GPL2
  */
@@ -29,6 +29,20 @@ if (!defined( 'Inpsyde\Wonolog\LOG')) {
 
 // Set default values here
 function wp_lagoon_logs_default_settings() {
+  // Check if WordPress is in the installation process
+  // wp_installing() returns true during installation process
+  if (function_exists('wp_installing') && wp_installing()) {
+    return;
+  }
+
+  // Check if the wp_options table exists before trying to update options
+  global $wpdb;
+  $table_exists = $wpdb->get_var("SHOW TABLES LIKE '{$wpdb->options}'");
+
+  if (!$table_exists) {
+    return;
+  }
+
   $default = [
     'll_settings_logs_host' => 'application-logs.lagoon.svc',
     'll_settings_logs_port' => 5140,
@@ -41,6 +55,19 @@ function wp_lagoon_logs_default_settings() {
  * Plugin init action because activation hook won't trigger in MU plugin.
  */
 function wp_lagoon_logs_extension_init() {
+  // Check if WordPress is in the installation process
+  if (function_exists('wp_installing') && wp_installing()) {
+    return;
+  }
+
+  // Check if the database is ready before accessing options
+  global $wpdb;
+  $table_exists = $wpdb->get_var("SHOW TABLES LIKE '{$wpdb->options}'");
+
+  if (!$table_exists) {
+    return;
+  }
+
   if (get_option( 'wp_ll_settings')) {
     return;
   }
@@ -49,6 +76,23 @@ function wp_lagoon_logs_extension_init() {
 add_action('init', 'wp_lagoon_logs_extension_init');
 
 if (getenv('LAGOON_ENVIRONMENT_TYPE') && getenv('LAGOON_ENVIRONMENT_TYPE') !== 'local') {
+  // Check if WordPress is in the installation process
+  if (function_exists('wp_installing') && wp_installing()) {
+    // Just bootstrap Wonolog during installation
+    Wonolog\bootstrap();
+    return;
+  }
+
+  // Check if the database is ready
+  global $wpdb;
+  $table_exists = $wpdb->get_var("SHOW TABLES LIKE '{$wpdb->options}'");
+
+  if (!$table_exists) {
+    // Just bootstrap Wonolog if database tables don't exist yet
+    Wonolog\bootstrap();
+    return;
+  }
+
   $options = get_option('wp_ll_settings', []);
 
   if (isset($options['ll_settings_logs_host'], $options['ll_settings_logs_port'], $options['ll_settings_logs_identifier'])) {
